6f54370cb2baa5d31370486f68af49e671dbf3a7
sreeshma <sreeshma.t.2019@gmail.com>
2024-06-14 14:28:11 +0530

    about update

--- frontend/src/components/pages/About/About.jsx
+++ frontend/src/components/pages/About/About.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 
 const About = () => {
-  return (
-   <div>
-   </div>
-  )
-}
+    return (
+        <div>
+            <div className="w-min-h-screen bg-pink-800 ">Hello</div>
+            <div className="flex bg-green-500 ">
+                <div className="w-1/2">
+                    <img src="https://experionglobal.com/wp-content/uploads/2024/02/Experion-Overview-730x1024.jpg" />
+                </div>
+                <div className="w-1/2">Right</div>
+            </div>
+        </div>
+    );
+};
 
-export default About
+export default About;
