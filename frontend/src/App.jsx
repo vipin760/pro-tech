8ba02c2dcffb24c109e5aacd550925c09b3470a4
sreeshma <sreeshma.t.2019@gmail.com>
2024-06-15 10:31:12 +0530

    about update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,37 @@
 import { Suspense, lazy, useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-const Home = lazy(()=>import("./components/pages/Home/Home"))
-const About = lazy(()=>import("./components/pages/About/About"))
-const Service = lazy(()=>import("./components/pages/Service/Service"))
-const Contact = lazy(()=>import("./components/pages/Contact/Contact"))
+const Home = lazy(() => import("./components/pages/Home/Home"));
+const About = lazy(() => import("./components/pages/About/About"));
+const Service = lazy(() => import("./components/pages/Service/Service"));
+const Contact = lazy(() => import("./components/pages/Contact/Contact"));
 import Header from "./components/partials/Header/Header";
 import Footer from "./components/partials/Footer/Footer";
-import logo from "../public/logo.png"
+import logo from "../public/logo.png";
 function App() {
-
-  return (
-    <>
-       <BrowserRouter>
-          <Header />
-          <div className="absolute z-10 bg-transparent  bg-opacity-50 w-full top-0 left-0 right-0">
-          <img src={logo} className="h-40 w-40 bg-transparent" alt="" srcset="" />
-          </div>
-          <Suspense fallback={<div>Loading.......</div>}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/About" element={<About/>} />
-              <Route path="/contact" element={<Contact/>} />
-              <Route path="/service" element={<Service/>} />
-            </Routes>
-          </Suspense>
-          <Footer/>
-    </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Header />
+                <div className="absolute z-10 bg-transparent  bg-opacity-50 w-full top-0 left-0 right-0">
+                    <img
+                        src={logo}
+                        className="h-40 w-40 bg-transparent"
+                        alt=""
+                    />
+                </div>
+                <Suspense fallback={<div>Loading.......</div>}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/About" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/service" element={<Service />} />
+                    </Routes>
+                </Suspense>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
