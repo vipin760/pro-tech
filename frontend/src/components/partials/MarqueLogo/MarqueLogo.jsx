2bca62eba349b7e65284f7a69bc9cd9cfe792888
vipin_760 <vipinm500@gmail.com>
2024-06-15 10:29:33 +0530

    service completed

--- frontend/src/components/partials/MarqueLogo/MarqueLogo.jsx
+++ frontend/src/components/partials/MarqueLogo/MarqueLogo.jsx
@@ -4,16 +4,7 @@ import Marquee from "react-fast-marquee";
 const MarqueLogo = () => {
   return (
     <div className=''>
-   <div className='md:flex my-16'>
-    <div className='w-full p-2 md:w-1/2'>
-        <h1>Clients & Data Centres</h1>
-        <h1 className='text-4xl'>Rendering services to Market leaders and global Entreprises</h1>
-    </div>
-    <div className='w-full p-2 md:w-1/2'>
-    OUR TEAM IS SERVING MANY LARGE AND MEDIUM GLOBAL FIRMS FOR THEIR INFRASTRUCTURE SERVICE NEEDS IN VARIOUS WELL-KNOWN DATA CENTRES AND INTERNET EXCHANGES. FAMILIARISATION WITH THEIR FACILITIES AND PROCEDURES BENEFITS OUR CLIENT IN EASING THEIR TASKS.
-    </div>
-   </div>
-
+        
    <Marquee className='bg-transparent'>
     <img className='bg-transparent mix-blend-color-burn object-contain' src="https://prohandstech.com/wp-content/uploads/2020/12/2.jpg" alt="" />
     <img className='bg-transparent mix-blend-color-burn object-contain' src="https://prohandstech.com/wp-content/uploads/2020/12/3.jpg" alt="" />
@@ -25,6 +16,15 @@ const MarqueLogo = () => {
     <img className='bg-transparent mix-blend-color-burn object-contain' src="https://prohandstech.com/wp-content/uploads/2020/12/8.jpg" alt="" />
     <img className='bg-transparent mix-blend-color-burn object-contain' src="https://prohandstech.com/wp-content/uploads/2020/12/9.jpg" alt="" />
 </Marquee>
+   <div className='md:flex my-16 lg:my-36'>
+    <div className='w-full p-2 md:w-1/2'>
+        <h1>Clients & Data Centres</h1>
+        <h1 className='text-4xl'>Rendering services to Market leaders and global Entreprises</h1>
+    </div>
+    <div className='w-full p-2 md:w-1/2'>
+    OUR TEAM IS SERVING MANY LARGE AND MEDIUM GLOBAL FIRMS FOR THEIR INFRASTRUCTURE SERVICE NEEDS IN VARIOUS WELL-KNOWN DATA CENTRES AND INTERNET EXCHANGES. FAMILIARISATION WITH THEIR FACILITIES AND PROCEDURES BENEFITS OUR CLIENT IN EASING THEIR TASKS.
+    </div>
+   </div>
     </div>
   )
 }
