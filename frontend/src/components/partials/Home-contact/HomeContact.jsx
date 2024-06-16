0e2c562cfce827887693414d4527c5d77382116b
vipin_760 <vipinm500@gmail.com>
2024-06-17 03:48:00 +0530

    contact us moved

--- frontend/src/components/partials/Home-contact/HomeContact.jsx
+++ frontend/src/components/partials/Home-contact/HomeContact.jsx
@@ -4,7 +4,7 @@ import FormData from '../FormData/FormData'
 const HomeContact = () => {
   return (
     <div className='p-4 flex flex-col items-center'>
-        <h1 className='text-sm m-3 font-bold text-opacity-35 md:text-center'>CONTACT US</h1>
+        <h1 className='text-sm m-3 font-bold text-opacity-35 md:text-center  w-full'>CONTACT US</h1>
         <h1 className='text-3xl text-center my-2'>Get in Touch</h1>
         <p className='max-w-4xl'>Ready to take your data center operations to the next level? Contact us today to learn
 more about our services and how we can help you achieve your business goals. Reach
